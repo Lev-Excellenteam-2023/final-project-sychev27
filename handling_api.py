ba25fe247fd6c210fd964e92d78eab049ef7ee13
alex <alexsychev27@gmail.com>
2023-06-04 10:54:40 +0300

    Created a text file that contains the api key

--- handling_api.py
+++ handling_api.py
@@ -2,20 +2,26 @@ import openai
 
 
 async def api_request(content, message):
-    openai.api_key = "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"
+    # Open and read the API key from the file
+    file = open("api_key.txt", 'r')
+    api_key = file.read()
 
+    # Set the OpenAI API key
+    openai.api_key = api_key
+
+    # Define the initial system message and user message
     messages = [{"role": "system", "content":
                  "You’re a good teacher who knows how to teach briefly according to topics written on the slides"
-                 " and if you  wrote on the topic don't write ot it again"}]
+                 " and if you wrote on the topic don't write it again"},
+                {"role": "user", "content": content + message}]
 
-    messages.append({"role": "user", "content": content + message})
-
+    # Send chat completion request to OpenAI API
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
 
+    # Return the generated response content
     return completion.choices[0].message.content
 
 
-#print(completion.choices[0].message.content)
